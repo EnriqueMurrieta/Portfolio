fd492bc67fc0f48b1d5d8165006a7db2474dcbbb
Enrique Murrieta <enrique_murrieta@hotmail.com>
2021-05-26 21:55:24 -0700

    Setting it up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import shagy from './images/shagy.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="big-image">
+        <div className="overlay">
+          <img className="shags"src={shagy} alt="Enrique"/>
+          <h1>Murrieta Acuna, Esteban Enrique</h1>    
+        </div>
+      </div>
     </div>
   );
 }
