c4e442524dfb1129c92df6300425ea67a5d15033
Enrique Murrieta <enrique_murrieta@hotmail.com>
2021-05-31 17:36:42 -0700

    Some UI changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
-import shagy from './images/shagy.jpg';/*
+import shagy from './images/shagy.jpg';
 import cuentas from './images/iphone.jpg';
 import cuentas2 from './images/cuentas.jpg';
-import bcmapa from './images/android.jpg';*/
+import bcmapa from './images/android.jpg';
 import principal from './images/principal.jpg';
 
 import trece from './images/capturas/13.PNG';
-import catorce from './images/capturas/14.jpg';/*
+import catorce from './images/capturas/14.jpg';
 import quince from './images/capturas/15.PNG';
 import dieciseis from './images/capturas/16.PNG';
 import diecisiete from './images/capturas/17.PNG';
@@ -16,10 +17,10 @@ import veinte from './images/capturas/20.PNG';
 import veintiuno from './images/capturas/21.PNG';
 import veintidos from './images/capturas/22.PNG';
 import veintitres from './images/capturas/23.PNG';
-import veinticuatro from './images/capturas/24.PNG';*/
+import veinticuatro from './images/capturas/24.PNG';
 
 import uno from './images/capturas/1.PNG';
-import dos from './images/capturas/2.PNG';/*
+import dos from './images/capturas/2.PNG';
 import tres from './images/capturas/3.PNG';
 import cuatro from './images/capturas/4.PNG';
 import cinco from './images/capturas/5.PNG';
@@ -29,14 +30,19 @@ import ocho from './images/capturas/8.PNG';
 import nueve from './images/capturas/9.PNG';
 import diez from './images/capturas/10.PNG';
 import once from './images/capturas/11.PNG';
-import doce from './images/capturas/12.PNG';*/
+import doce from './images/capturas/12.PNG';
 
 //CROSS BROWSER COMPATIBILITY
 
 function App() {
-  const verMas = () => {
-    console.log("Heeey")
+  const [verMas, onVerMas] = React.useState(false);
+  const [boton, onBoton] = React.useState("Ver más capturas");
+
+  const verMasFunction = () => {
+    boton == "Ver menos" ? onBoton("Ver más capturas") : onBoton("Ver menos");
+    onVerMas(!verMas);
   }
+
   return (
     <div>
       <div className="big-image">
@@ -74,7 +80,7 @@ function App() {
                 <img className="capturas" src={trece} alt="13" />
                 <img className="capturas" src={catorce} alt="14" />
               </div>
-              
+              <p className="footNote">(2 / 12)</p>
           </div>
           <div className="contained2">
             <div className="title">
@@ -92,12 +98,87 @@ function App() {
                 <img className="capturas" src={uno} alt="1" />
                 <img className="capturas" src={dos} alt="2" />
             </div>
-            
+            <p className="footNote">(2 / 12)</p>
           </div>
         </div>
-        <div className="verMas">
-          <button className="verMasBotton" onClick={verMas}>Ver más capturas</button>
-        </div>
+        {verMas ? 
+          <div>
+            <div className="container2">
+              <div className="contained2Left">
+                <div className="title">
+                  <h1>Cuentas</h1>
+                </div>
+                <div className="capturasPacket">
+                  <img className="capturas" src={quince} alt="15" />
+                  <img className="capturas" src={dieciseis} alt="16" />
+                </div>
+                <p className="footNote">(4 / 12)</p>
+                <div className="capturasPacket">
+                  <img className="capturas" src={diecisiete} alt="17" />
+                  <img className="capturas" src={dieciocho} alt="18" />
+                </div>
+                <p className="footNote">(6 / 12)</p>
+                <div className="capturasPacket">
+                  <img className="capturas" src={diecinueve} alt="19" />
+                  <img className="capturas" src={veinte} alt="20" />
+                </div>
+                <p className="footNote">(8 / 12)</p>
+                <div className="capturasPacket">
+                  <img className="capturas" src={veintiuno} alt="21" />
+                  <img className="capturas" src={veintidos} alt="22" />
+                </div>
+                <p className="footNote">(10 / 12)</p>
+                <div className="capturasPacket">
+                  <img className="capturas" src={veintitres} alt="23" />
+                  <img className="capturas" src={veinticuatro} alt="24" />
+                </div>
+                <p className="footNote">(12 / 12)</p>
+                <img src={cuentas2} alt="Enrique"/>
+                <img src={cuentas} alt="Enrique"/>
+              </div>
+              <div className="contained2">
+                <div className="title">
+                  <h1>BCMapa</h1>
+                </div>
+                <div className="capturasPacket">
+                  <img className="capturas" src={tres} alt="3" />
+                  <img className="capturas" src={cuatro} alt="4" />
+                </div>
+                <p className="footNote">(4 / 12)</p>
+                <div className="capturasPacket">
+                  <img className="capturas" src={cinco} alt="5" />
+                  <img className="capturas" src={seis} alt="6" />
+                </div>
+                <p className="footNote">(6 / 12)</p>
+                <div className="capturasPacket">
+                  <img className="capturas" src={siete} alt="7" />
+                  <img className="capturas" src={ocho} alt="8" />
+                </div>
+                <p className="footNote">(8 / 12)</p>
+                <div className="capturasPacket">
+                  <img className="capturas" src={nueve} alt="9" />
+                  <img className="capturas" src={diez} alt="10" />
+                </div>
+                <p className="footNote">(10 / 12)</p>
+                <div className="capturasPacket">
+                  <img className="capturas" src={once} alt="11" />
+                  <img className="capturas" src={doce} alt="12" />
+                </div>
+                <p className="footNote">(12 / 12)</p>
+                <img src={bcmapa} alt="Enrique"/>
+              </div>
+            </div>
+            <div className="verMas">
+              <button className="verMasBotton" onClick={verMasFunction}>{boton}</button>
+            </div>
+          </div>
+        : 
+          <div>
+            <div className="verMas">
+              <button className="verMasBotton" onClick={verMasFunction}>{boton}</button>
+            </div>
+          </div>
+        }
         <h1>Proximamente...</h1>
         <h2>App de noticias</h2>
         <h2>Refactorizacion de Cuentas y App de noticias con redux</h2>
@@ -107,52 +188,3 @@ function App() {
 }
 
 export default App;
-
-/*
-<div className="capturasPacket">
-                <img className="capturas" src={quince} alt="15" />
-                <img className="capturas" src={dieciseis} alt="16" />
-              </div>
-              <div className="capturasPacket">
-                <img className="capturas" src={diecisiete} alt="17" />
-                <img className="capturas" src={dieciocho} alt="18" />
-              </div>
-              <div className="capturasPacket">
-                <img className="capturas" src={diecinueve} alt="19" />
-                <img className="capturas" src={veinte} alt="20" />
-              </div>
-              <div className="capturasPacket">
-                <img className="capturas" src={veintiuno} alt="21" />
-                <img className="capturas" src={veintidos} alt="22" />
-              </div>
-              <div className="capturasPacket">
-                <img className="capturas" src={veintitres} alt="23" />
-                <img className="capturas" src={veinticuatro} alt="24" />
-              </div>
-              <img src={cuentas2} alt="Enrique"/>
-              <img src={cuentas} alt="Enrique"/>
-*/
-
-/*
-<div className="capturasPacket">
-                <img className="capturas" src={tres} alt="3" />
-                <img className="capturas" src={cuatro} alt="4" />
-            </div>
-            <div className="capturasPacket">
-                <img className="capturas" src={cinco} alt="5" />
-                <img className="capturas" src={seis} alt="6" />
-            </div>
-            <div className="capturasPacket">
-                <img className="capturas" src={siete} alt="7" />
-                <img className="capturas" src={ocho} alt="8" />
-            </div>
-            <div className="capturasPacket">
-                <img className="capturas" src={nueve} alt="9" />
-                <img className="capturas" src={diez} alt="10" />
-            </div>
-            <div className="capturasPacket">
-                <img className="capturas" src={once} alt="11" />
-                <img className="capturas" src={doce} alt="12" />
-            </div>
-            <img src={bcmapa} alt="Enrique"/>
-*/
